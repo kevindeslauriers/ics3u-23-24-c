3126513f70478f8719b50ab9d66e07b86ff44cf8
Kevin DesLauriers <kdeslauriers@bayviewglen.ca>
2023-09-11 10:57:02 -0400

    Changed Example One

--- Unit One - Introduction to Java/src/lesson2/ExampleOne.java
+++ Unit One - Introduction to Java/src/lesson2/ExampleOne.java
@@ -3,5 +3,6 @@ package lesson2;
 public class ExampleOne {
     public static void main(String[] args) {
         System.out.println("Hello ICS3U!");
+        System.out.println("Goodbye ICS3U!");
     }
 }
